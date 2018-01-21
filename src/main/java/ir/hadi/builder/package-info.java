5c29d37de49e8819e5c65460d3f47c2d447b66ff
hadi <rahnemon2011@gmail.com>
2018-01-21 10:23:27 +0100

    Add package-info for builder package

---
+++ src/main/java/ir/hadi/builder/package-info.java
@@ -0,0 +1,7 @@
+/**
+ * Creating builder design pattern can be implemented by two ways: normal and nested.
+ * nested is more cleaner but more complicated to create.
+ *
+ * @author hadi
+ */
+package ir.hadi.builder;
